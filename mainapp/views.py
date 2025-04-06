057c6bea827e6faac8b4478654e9c4f96c16d89e
Anvar <anvar7235@gmail.com>
2025-04-06 15:12:09 +0500

    Added templates and static files

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Привет! Это мой первый Django-сайт!")
+    return render(request, 'mainapp/index.html')
